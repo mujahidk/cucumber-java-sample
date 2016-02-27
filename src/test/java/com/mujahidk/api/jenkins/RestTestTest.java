ca672d22d782aa101e96c4fbd734441d30b96b28
Mujahid Khaleel <mujahidkhaleel@gmail.com>
2016-02-27 15:29:32 -0500

    Add first sample

---
+++ src/test/java/com/mujahidk/api/jenkins/RestTestTest.java
@@ -0,0 +1,10 @@
+package com.mujahidk.api.jenkins;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/** To run with JUnit **/
+@RunWith(Cucumber.class)
+public class RestTestTest
+{
+}
